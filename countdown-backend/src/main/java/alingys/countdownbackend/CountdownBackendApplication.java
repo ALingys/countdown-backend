fdea59361b984826f8fbb8b01eae16342e1f46dd
Augustas Lingys <augustas.lingys@gmail.com>
2023-03-26 18:59:59 +0300

    Init commit

---
+++ countdown-backend/src/main/java/alingys/countdownbackend/CountdownBackendApplication.java
@@ -0,0 +1,13 @@
+package alingys.countdownbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountdownBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountdownBackendApplication.class, args);
+	}
+
+}
---
+++ countdown-backend/src/test/java/alingys/countdownbackend/CountdownBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package alingys.countdownbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountdownBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
